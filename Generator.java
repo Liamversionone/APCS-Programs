c219c38f2a8acabad5963d44ecfd195799c67e0c
Liamversionone <91441032+Liamversionone@users.noreply.github.com>
2022-05-25 08:51:03 -0400

    Adding comments

--- Generator.java
+++ Generator.java
@@ -1,23 +1,31 @@
 package mod;
 
 public class Generator {
-
+	
+	/*These instance variables are used to store data for the number of correct and incorrect questions that have been answered,
+	  along with the correct answer for the current question.*/
 	private int correct;
 	private int incorrect;
 	private int answer;
 	
+	//These methods give access to the values that are stored within the private instance variables.
 	public int getCorrect() {return correct;}
 	public int getIncorrect() {return incorrect;}
 	public int getAnswer() {return answer;}
 	
+	//These setters allow for the incrementation of the instance varaibles from outside classes.
 	public void addC() {correct++;}
 	public void addI() {incorrect++;}
 	
+	//The constructor sets the "correct" and "incorrect" instance variables to be 0, as no questions have been answered yet.
 	public Generator() {
 		correct = 0;
 		incorrect = 0;
 	}
 	
+	/*This method is used to generate a new addition problem. It will create the two numbers from 0 - 100. The answer to the question is
+	  the greater of the two numbers, and is stored in the "answer" instance variable. It will return the finished equation, which is
+	  (bigger number - smaller number) + smaller number = bigger number.*/
 	public String generateAddition() {
 		int num1 = ((int) (Math.random() * 10)) + (((int) (Math.random() * 10)) * 10);
 		if(Math.random() < 0.50)
@@ -34,6 +42,7 @@ public class Generator {
 			return num2 - num1 + " + " + num1 + " = ";
 		}
 	}
+	
 	
 	public String generateSubtraction() {
 		int num1 = ((int) (Math.random() * 10)) + (((int) (Math.random() * 10)) * 10);
@@ -52,6 +61,7 @@ public class Generator {
 		}
 	}
 	
+	
 	public String generateMultiplication() {
 		int num1 = ((int) (Math.random() * 10)) + (((int) (Math.random() * 10)) * 10);
 		if(Math.random() < 0.50)
@@ -65,6 +75,8 @@ public class Generator {
 		return answer / num2 + " \u00D7 " + num2 + " = ";
 	}
 	
+	/*This method is used to generate a new division problem. 
+	  */
 	public String generateDivision() {
 		int num1 = ((int) (Math.random() * 10)) + (((int) (Math.random() * 10)) * 10);
 		if(Math.random() < 0.50)
@@ -76,6 +88,7 @@ public class Generator {
 		return (num1 * num2) + " \u00F7 " + num2 + " = ";
 	}
 	
+	//This method will choose a random math problem to generate each time it is called. 
 	public String generateRandom() {
 		double x = Math.random();
 		if(x < 0.25) {
