33e6f5643308e54a149a42357104c3fb3b997eb2
Liamversionone <91441032+Liamversionone@users.noreply.github.com>
2022-05-31 17:46:54 -0400

    Update Generator.java

--- Generator.java
+++ Generator.java
@@ -43,7 +43,8 @@ public class Generator {
 		}
 	}
 	
-	
+	/*This method is used to generate a new subtraction problem. The method generates two int variables with random values. It will determine which value
+	  is greater, and set it as the answer to the equation. The returned equation is (bigger number + smaller number) - (smaller number) = (bigger number).*/
 	public String generateSubtraction() {
 		int num1 = ((int) (Math.random() * 10)) + (((int) (Math.random() * 10)) * 10);
 		if(Math.random() < 0.50)
@@ -61,7 +62,9 @@ public class Generator {
 		}
 	}
 	
-	
+	/*This method is used to generate a new multiplication problem. The method creates two random int variables, one with a value from 0-100, and the second
+	  with a value from 0-10. The first variable is the answer to the equation. The second variable is used to divide the first variable to find the other
+	  factor in the equation. The returned equation is (bigger number / smaller number) * (smaller number) = (bigger number).*/
 	public String generateMultiplication() {
 		int num1 = ((int) (Math.random() * 10)) + (((int) (Math.random() * 10)) * 10);
 		if(Math.random() < 0.50)
@@ -75,8 +78,9 @@ public class Generator {
 		return answer / num2 + " \u00D7 " + num2 + " = ";
 	}
 	
-	/*This method is used to generate a new division problem. 
-	  */
+	/*This method is used to generate a new division problem. The method creates two random int variables with values from 0-100. The first int generated is used
+	  as the answer to the equation. The dividend of the equation is found by multiplying the two random ints together. The returned equation is 
+	  (number1 * number2) / (number2) = (number1).*/
 	public String generateDivision() {
 		int num1 = ((int) (Math.random() * 10)) + (((int) (Math.random() * 10)) * 10);
 		if(Math.random() < 0.50)
