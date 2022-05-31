f246b261dd91a880bce3fb1bdba2336bb298acea
Liamversionone <91441032+Liamversionone@users.noreply.github.com>
2022-05-31 17:47:33 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package main;
 import mod.Overseer;
 
 public class Main {
+	//This method is used to run and execute the program.
 	public static void main(String[] args0) {
 		Overseer over = new Overseer();
 	}
